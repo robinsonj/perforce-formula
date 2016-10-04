bcfefb1e352db8440fe1b95dcfa58663503cd0a4
Joseph L. Robinson <robinsonjosephlewis@gmail.com>
2016-10-04 14:17:34 -0700

    Use the /opt/perforce PATH if necessary
    On some installs, executables are only available in
    /opt/perforce/[s]bin. Add this to the path as necessary.

--- _modules/p4broker.py
+++ _modules/p4broker.py
@@ -1,11 +1,22 @@
+from __future__ import absolute_import
+
 import re
+import os
+
+# Import salt libs.
+import salt.utils
 
 def _p4broker(opts, env=None):
     '''
     '''
 
-    p4broker = 'p4broker'
-    cmd      = '{0} {1}'.format(p4broker, opts)
+    p4broker = salt.utils.which('p4broker')
+
+    if p4broker == None and "/opt/perforce/sbin" not in os.environ["PATH"]:
+        os.environ["PATH"] += os.pathsep + "/opt/perforce/sbin"
+        p4broker = salt.utils.which('p4broker')
+
+    cmd = '{0} {1}'.format(p4broker, opts)
 
     return __salt__['cmd.run'](cmd, python_shell=False, env=env)
 
